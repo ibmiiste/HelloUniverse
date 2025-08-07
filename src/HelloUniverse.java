8d49b2ea086ef3fa7d65ad0bd2ac478873526583
Olivier Drubigny <olivier.drubigny@ista.com>
2025-08-07 09:07:25 +0200

    Corrigé Le cas de pluton Version 3 - Incrémenter le nombre de planètes
    Reprenez HelloUniverse.java et faites en sortes que la variable qui représente le nombre de planètes passe de 8 à 9 entre les 2 affichages grâce à l'opérateur d'incrémentation.

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,12 +1,22 @@
+/**
+ * Reprenez HelloUniverse.java et faites en sortes que la variable qui représente
+ * le nombre de planètes passe de 8 à 9 entre les 2 affichages grâce à l'opérateur d'incrémentation.
+ */
+
 public class HelloUniverse {
-    public static void main(String... arg){
+
+    /**
+     * Point d'entrée du programme
+     * @param args arguments passés au programme
+     */
+    public static void main(String... args) {
         String messageDebut = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
         System.out.println(messageDebut);
         int nombrePlanetes = 8;
         System.out.println(nombrePlanetes);
         String messageSuite = "Il y a quelques années cependant, elles étaient au nombre de : ";
         System.out.println(messageSuite);
-        nombrePlanetes = 9;
+        nombrePlanetes++;
         System.out.println(nombrePlanetes);
     }
 }
