1e7baa3e0fa907a98efb2b2fd59eafe8667c634d
Olivier Drubigny <olivier.drubigny@ista.com>
2025-08-08 08:53:47 +0200

    Corrigé Exercice de codage 2 : Le cas de Pluton - Première variable

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,3 +1,17 @@
+/** Le cas de Pluton - Première variable
+ Les scientifiques remettent régulièrement en question le nombre de planètes dans le système solaire. 
+ Actuellement au nombre de 8, on en comptabilisait 9 avec Pluton il y a encore quelques années. 
+ L'objectif est de reprendre le code "Hello Universe" en affichant successivement :
+ 
+    Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : 
+ 
+        <La valeur d'une variable de type int >
+
+        Il y a quelques années cependant, elles étaient au nombre de : 
+
+        <La valeur de la MÊME VARIABLE>
+*/
+
 public class HelloUniverse {
     public static void main(String... arg){
         System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
