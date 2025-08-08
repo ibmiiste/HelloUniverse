68d0d12a2e2823b3fab398845779652f0a9b4626
Olivier Drubigny <olivier.drubigny@ista.com>
2025-08-08 08:49:04 +0200

    Ajout de commentaires explicatifs pour le programme HelloUniverse

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,3 +1,14 @@
+/**
+   Créez le programme HelloUniverse (New -> Java Class) et remplacez le code généré par le modèle suivant :
+    public class HelloUniverse {
+        public static void main(String... args){
+     
+        }
+    }
+   Adaptez le code de manière à ce qu'il affiche dans la console de sortie :
+   Hello Universe!
+*/
+Exécutez.
 public class HelloUniverse {
     public static void main(String... arg){
         System.out.println("Hello Universe!");
