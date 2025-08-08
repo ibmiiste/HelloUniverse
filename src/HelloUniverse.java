e0f4a686044b2eecc4a3227589c4013287b3a9bb
Olivier Drubigny <olivier.drubigny@ista.com>
2025-08-08 08:51:06 +0200

    Corrigé Exercice de codage 1 : Hello Universe

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,15 +1,16 @@
 /**
-   Créez le programme HelloUniverse (New -> Java Class) et remplacez le code généré par le modèle suivant :
-    public class HelloUniverse {
-        public static void main(String... args){
-     
-        }
-    }
-   Adaptez le code de manière à ce qu'il affiche dans la console de sortie :
-   Hello Universe!
+ * Corrigé Exercice de codage 1 : Hello Universe
+ * Créez le programme HelloUniverse (New -> Java Class) et remplacez le code généré par le modèle suivant :
+ *  public class HelloUniverse {
+ *      public static void main(String... args){
+ *    
+ *      }
+ *  }
+ * Adaptez le code de manière à ce qu'il affiche dans la console de sortie :
+ * Hello Universe!
+ * Exécutez.
 */
-Exécutez.
-public class HelloUniverse {
+Public class HelloUniverse {
     public static void main(String... arg){
         System.out.println("Hello Universe!");
     }
