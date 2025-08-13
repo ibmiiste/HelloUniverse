353b67b9e6fba044c950797727fd5e1df658df54
Olivier Drubigny <olivier.drubigny@ista.com>
2025-08-13 09:11:08 +0200

    Corrigé Exercice de codage 12 : Galilée où la réalité des planètes - Définir le concept de planète

---
+++ src/Planete.java
@@ -0,0 +1,10 @@
+/**
+ * Galilée où la réalité des planètes - Définir le concept de planète
+ *
+ * Galilée fut le premier à observer grâce à une lunette les planètes dans le ciel. S'il ne les a pas découvertes, on peut tout de même dire qu'il en a confirmé la réalité.
+ *
+ * Nous allons nous aussi confirmer la réalité de ce concept en décrivant la classe Planete  dans notre logiciel.
+*/
+public class Planete {
+    
+}
